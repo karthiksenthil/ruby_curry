386374620512b18df790811d8a8e47626af78916
Karthik Senthil <karthik.senthil94@gmail.com>
2015-06-01 23:57:14 +0200

    Added naive reverse example

--- examples/append.rb
+++ examples/append.rb
@@ -10,31 +10,31 @@ require_relative '../src/definitional_tree.rb'
 
 
 # Symbols in the rules
-append_symbol = XSymbol.new("append",2,:oper)
-nil_list_symbol = XSymbol.new("[]",0,:ctor)
-cons_symbol = XSymbol.new(":",2,:ctor)
+@append_symbol = XSymbol.new("append",2,:oper)
+@nil_list_symbol = XSymbol.new("[]",0,:ctor)
+@cons_symbol = XSymbol.new(":",2,:ctor)
 
 # Variables in the rules
-xs = Variable.new("xs")
+@xs = Variable.new("xs")
 ys = Variable.new("ys")
 z = Variable.new("z")
 zs = Variable.new("zs")
 
 # child1 i.e rule1 ; lhs = pattern and rhs = expression
-lhs1 = Application.new(append_symbol,[nil_list_symbol,ys])
+lhs1 = Application.new(@append_symbol,[@nil_list_symbol,ys])
 rhs1 = ys	
 child1 = Leaf.new(lhs1,rhs1)
 
 # child2 i.e rule2
 # (z:zs) itself is another sub-pattern which is built using the : symbol
-lhs2 = Application.new(append_symbol,[Application.new(cons_symbol,[z,zs]),ys])
+lhs2 = Application.new(@append_symbol,[Application.new(@cons_symbol,[z,zs]),ys])
 # similarly (append zs ys)
-rhs2 = Application.new(cons_symbol,[z,Application.new(append_symbol,[zs,ys])])
+rhs2 = Application.new(@cons_symbol,[z,Application.new(@append_symbol,[zs,ys])])
 child2 = Leaf.new(lhs2,rhs2)
 
 # definitional tree for above rules
 
-rootpatt = Application.new(append_symbol,[xs,ys])
-append_tree = Branch.new(rootpatt,xs,[child1,child2])
+rootpatt = Application.new(@append_symbol,[@xs,ys])
+append_tree = Branch.new(rootpatt,@xs,[child1,child2])
 append_tree.pretty_print()
 print "\n"
---
+++ examples/naive_reverse.rb
@@ -0,0 +1,43 @@
+require_relative '../src/definitional_tree.rb'
+
+# Example to construct definitonal trees for a curry program
+# to reverse a list
+# Program link : http://www-ps.informatik.uni-kiel.de/smap/smap.cgi?browser/1
+
+# 2 operations : append & rev
+
+# Rules for append -
+# General rule -> append xs ys
+# 1. append [] ys = ys
+# 2. append (z:zs) ys = z:(append zs ys)
+
+# Rules for rev -
+# General rule -> rev xs
+# 1. rev [] = []
+# 2. rev (x:xs) = append (rev xs) [x]
+
+# import the append operation's definitional tree from append.rb
+require_relative 'append.rb'
+
+# Symbol for reverse operation
+reverse_symbol = XSymbol.new("rev",1,:oper)
+
+# Variable
+x = Variable.new("x")
+
+# rule 1
+lhs1 = Application.new(reverse_symbol,[@nil_list_symbol])
+rhs1 = @nil_list_symbol
+child1 = Leaf.new(lhs1,rhs1)
+
+# rule 2
+lhs2 = Application.new(reverse_symbol,[Application.new(@cons_symbol,[x,@xs])])
+rhs2 = Application.new(@append_symbol,[Application.new(reverse_symbol,[@xs]),x])
+child2 = Leaf.new(lhs2,rhs2)
+
+
+rev_tree_rootpatt = Application.new(reverse_symbol,[@xs])
+rev_tree_rootnode = Branch.new(rev_tree_rootpatt,@xs,[child1,child2])
+rev_tree_rootnode.pretty_print()
+print "\n"
+
