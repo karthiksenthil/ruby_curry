1f44e833b3415a69c69e87cbf4889f36753db74a
Karthik Senthil <karthik.senthil94@gmail.com>
2015-06-02 08:46:08 +0200

    Added last element of list example

--- examples/append.rb
+++ examples/append.rb
@@ -16,25 +16,25 @@ require_relative '../src/definitional_tree.rb'
 
 # Variables in the rules
 @xs = Variable.new("xs")
-ys = Variable.new("ys")
+@ys = Variable.new("ys")
 z = Variable.new("z")
 zs = Variable.new("zs")
 
 # child1 i.e rule1 ; lhs = pattern and rhs = expression
-lhs1 = Application.new(@append_symbol,[@nil_list_symbol,ys])
-rhs1 = ys	
+lhs1 = Application.new(@append_symbol,[@nil_list_symbol,@ys])
+rhs1 = @ys	
 child1 = Leaf.new(lhs1,rhs1)
 
 # child2 i.e rule2
 # (z:zs) itself is another sub-pattern which is built using the : symbol
-lhs2 = Application.new(@append_symbol,[Application.new(@cons_symbol,[z,zs]),ys])
+lhs2 = Application.new(@append_symbol,[Application.new(@cons_symbol,[z,zs]),@ys])
 # similarly (append zs ys)
-rhs2 = Application.new(@cons_symbol,[z,Application.new(@append_symbol,[zs,ys])])
+rhs2 = Application.new(@cons_symbol,[z,Application.new(@append_symbol,[zs,@ys])])
 child2 = Leaf.new(lhs2,rhs2)
 
 # definitional tree for above rules
 
-rootpatt = Application.new(@append_symbol,[@xs,ys])
+rootpatt = Application.new(@append_symbol,[@xs,@ys])
 append_tree = Branch.new(rootpatt,@xs,[child1,child2])
 append_tree.pretty_print()
 print "\n"
---
+++ examples/last_element_list.rb
@@ -0,0 +1,61 @@
+require_relative '../src/definitional_tree.rb'
+
+# Example to construct definitonal trees for a curry program
+# to determine last element of a list
+# Program link : http://www-ps.informatik.uni-kiel.de/smap/smap.cgi?browser/2
+
+# 3 operations : append, last & cond
+
+# Rules for append -
+# General rule -> append xs ys
+# 1. append [] ys = ys
+# 2. append (z:zs) ys = z:(append zs ys)
+
+# Rules for rev -
+# General rule -> last xs
+# 1. last xs = cond (append xs x =:= xs) x
+
+# Rules for cond -
+# General rule -> cond xs ys
+# 1. cond success ys = ys
+
+# here =:= is constained equality operator
+
+# import the append operation's definitional tree from append.rb
+require_relative 'append.rb'
+
+# Symbols
+last_symbol = XSymbol.new("last",1,:oper)
+condition_symbol = XSymbol.new("cond",2,:oper)
+constrained_equal = XSymbol.new("=:=",2,:oper)
+success_symbol = XSymbol.new("success",1,:ctor)
+
+# Variables
+x = Variable.new("x")
+
+# definitional tree for last operation
+last_tree_rootlhs = Application.new(last_symbol,[@xs])
+last_tree_rootrhs = Application.new(condition_symbol,[Application.new(constrained_equal,
+											[Application.new(@append_symbol,[@xs,x]),@xs]),x])
+
+last_tree_rootnode = Leaf.new(last_tree_rootlhs,last_tree_rootrhs)
+last_tree_rootnode.pretty_print()
+print "\n"
+
+# definitional tree for condition operation
+
+@ys.show()
+cond_lhs1 = Application.new(condition_symbol,[success_symbol,@ys])
+cond_rhs1 = @ys
+child1 = Leaf.new(cond_lhs1,cond_rhs1)
+
+child1.pretty_print()
+
+cond_tree_rootpatt = Application.new(condition_symbol,[@xs,@ys])
+cond_tree_rootnode = Branch.new(cond_tree_rootpatt,@xs,[child1])
+# cond_tree_rootnode.pretty_print()
+print "\n"
+
+
+
+
