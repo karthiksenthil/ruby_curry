acb7586b12a4d08b1bda3ad2a76f23d3b4504544
Karthik Senthil <karthik.senthil94@gmail.com>
2016-06-17 01:47:47 +0530

    Updated object code produced by latest version

--- lib/mynot.rb
+++ lib/mynot.rb
@@ -1,58 +1,55 @@
+module Mynot
 
-require 'src/runtime/function_N.rb'
-require 'src/runtime/function_A.rb'
-require 'src/compiler/expressions.rb'
-require 'src/compiler/symbols.rb'
-require 'src/compiler/utilities.rb'
-require 'src/compiler/repl.rb'
+  require 'src/runtime/function_N.rb'
+  require 'src/runtime/function_A.rb'
+  require 'src/compiler/expressions.rb'
+  require 'src/compiler/symbols.rb'
+  require 'src/compiler/utilities.rb'
+  require 'src/compiler/repl.rb'
+  require 'System.rb'
+  require 'integers.rb'
+  require 'External.rb'
 
-#Object code for mynot module
-$_ct_myFalse = Constructor.new('_ct_myFalse',0)
-$_ct_myFalse.token_value = 4
-$_ct_myTrue = Constructor.new('_ct_myTrue',0)
-$_ct_myTrue.token_value = 5
-$_ct_myNot = Operation.new('_ct_myNot',1,nil)
-$_ct_main = Operation.new('_ct_main',0,nil)
+  require 'Prelude'
 
+  Mynot::CT_myFalse = Constructor.new('Mynot::CT_myFalse',0)
+  Mynot::CT_myFalse.token_value = 4
+  Mynot::CT_myTrue = Constructor.new('Mynot::CT_myTrue',0)
+  Mynot::CT_myTrue.token_value = 5
 
-#defining H function for _ct_myNot operation
-def $_ct_myNot.H(expr)
-  var1 = expr.content.arguments[0]
-  #var2 is Unimplemented. Could be ICase, IBind, IFree
-  var2 = var1
-  case var2.content.symbol.token
-  when 0 # VARIABLE
-    raise 'Handling Variables not implemented yet'
-  when 1 # CHOICE
-    var2.H()
-    expr.H()
-  when 2 # FAIL
-    expr.replace(Box.new(Application.new($_ct_fail,[])).content)
-  when 3 # OPERATION
-    var2.H()
-    expr.H()
-  when 4 # "myFalse" => _ct_myFalse
-    rhs = Box.new(Application.new($_ct_myTrue,[]))
-    expr.replace(rhs.content)
-  when 5 # "myTrue" => _ct_myTrue
-    rhs = Box.new(Application.new($_ct_myFalse,[]))
-    expr.replace(rhs.content)
+  Mynot::CT_myNot = Operation.new('Mynot::CT_myNot',1) # "myNot"
+  Mynot::CT_main = Operation.new('Mynot::CT_main',0) # "main"
+
+  def CT_myNot.H(expr) # "myNot"
+    var1 = expr.content.arguments[0]
+    #var2 is Unimplemented. Could be ICase, IBind, IFree
+    var2 = var1
+    case var2.content.symbol.token
+    when 0 # VARIABLE
+      raise 'Handling Variables not implemented yet'
+    when 1 # CHOICE
+      var2.H()
+      expr.H()
+    when 2 # FAIL
+      expr.replace(Box.new(Application.new(Default::CT_fail,[])).content)
+    when 3 # OPERATION
+      var2.H()
+      expr.H()
+    when 4 # "myFalse" => CT_myFalse
+      rhs = Box.new(Application.new(Mynot::CT_myTrue,[]))
+      expr.replace(rhs.content)
+    when 5 # "myTrue" => CT_myTrue
+      rhs = Box.new(Application.new(Mynot::CT_myFalse,[]))
+      expr.replace(rhs.content)
+    end
   end
+
+  def CT_main.H(expr) # "main"
+    rhs = Box.new(Application.new(Mynot::CT_myNot,[Box.new(Application.new(Mynot::CT_myNot,[Box.new(Application.new(Mynot::CT_myTrue,[]))]))]))
+    expr.replace(rhs.content)
+    expr.H()
+  end
+
+  repl(Box.new(Application.new(Mynot::CT_main,[])))
+
 end
-
-
-#defining H function for _ct_main operation
-def $_ct_main.H(expr)
-  rhs = Box.new(Application.new($_ct_myNot,[Box.new(Application.new($_ct_myNot,[Box.new(Application.new($_ct_myTrue,[]))]))]))
-  expr.replace(rhs.content)
-  expr.H()
-end
-
-def main
-  main_expr = Box.new(Application.new($_ct_main,[]))
-  repl(main_expr)
-end
-
-main
-
-#end of module
