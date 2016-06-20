e3b2232ed5a4223d883dc831b641f9964be777ff
Karthik Senthil <karthik.senthil94@gmail.com>
2016-06-21 02:16:31 +0530

    Changed test system to verify from output files.

--- test/test_positives.rb
+++ test/test_positives.rb
@@ -1,11 +1,6 @@
 require 'minitest'
 require 'minitest/autorun'
 require 'colorize'
-require_relative './positives_expected_output.rb'
-
-def compare_output(op)
-  return $expected_output == op
-end
 
 class TestPositivePrograms < Minitest::Test
 
@@ -14,28 +9,41 @@ class TestPositivePrograms < Minitest::Test
 
     failing_progs = ['addition.curry','bintree.curry','casecolor.curry',
                       'example_fill.curry','fibo.curry','letrec.curry']
-    all_progs = `ls #{basedir}/curry_examples/`.split
+    all_progs = `ls #{basedir}/curry_examples/ | egrep '\.curry'`.split
 
     passing_progs = all_progs - failing_progs
 
     # TODO : support for logging compiler output
 
-    obtained_output = {}
+    obtained_matches = {}
+    expected_matches = {}
     passing_progs.each do |prog|
       # puts "Started test on #{prog}"
+      expected_matches[prog] = true
       #compile curry to icur
       `#{basedir}/bin/curry2icur -f #{basedir}/curry_examples/#{prog}`
       #convert icur to ruby object code
       `#{basedir}/bin/icur2ruby -f #{basedir}/curry_examples/#{prog}`
       #execute object code and save output
-      output = `#{basedir}/bin/exec #{basedir}/curry_examples/#{prog}`
-      obtained_output[prog] = output
-      print ".".green
+      obt_output = `#{basedir}/bin/exec #{basedir}/curry_examples/#{prog}`.chomp
+
+      #load expected output
+      expected_output_file = "#{basedir}/curry_examples/" + 
+                              File.basename(prog,".curry") + ".output"
+      
+      if File.exist?(expected_output_file)
+        exp_output = File.open(expected_output_file).read()
+        obtained_matches[prog] = exp_output == obt_output
+        print ".".green
+      else
+        puts "\nExpected output file for #{prog} not found"
+        obtained_matches[prog] = false
+        print ".".red
+      end
     end
 
-    assert_equal($expected_output,obtained_output,"Positive programs test failed.")
+    assert_equal(expected_matches,obtained_matches,"Positive programs test failed.")
   end
 
 end
-
 
