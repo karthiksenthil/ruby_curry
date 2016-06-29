ff4ddc08d35fa7cb81a4013d9f2a229730e888e5
Karthik Senthil <karthik.senthil94@gmail.com>
2016-06-30 02:39:37 +0530

    Updated examples with latest version object code.

--- examples/Append.rb
+++ examples/Append.rb
@@ -2,12 +2,13 @@ module Append
 
   require 'src/runtime/function_N.rb'
   require 'src/runtime/function_A.rb'
-  require 'src/compiler/expressions.rb'
+  require 'src/compiler/expressions_include.rb'
   require 'src/compiler/symbols.rb'
   require 'src/compiler/utilities.rb'
   require 'src/compiler/repl.rb'
   require 'CT_System.rb'
   require 'integers.rb'
+  require 'CT_Character.rb'
   require 'CT_External.rb'
 
   require 'Prelude'
@@ -18,39 +19,42 @@ module Append
   def CT_append.H(expr) # "append"
     var1 = expr.content.arguments[0]
     var2 = expr.content.arguments[1]
-    #var5 is Unimplemented. Could be ICase, IBind, IFree
+    # var5 case selector
     var5 = var1
-    case var5.content.symbol.token
-    when 0 # VARIABLE
-      raise 'Handling Variables not implemented yet'
-    when 1 # CHOICE
-      var5.H()
-      expr.H()
-    when 2 # FAIL
-      expr.replace(Box.new(Application.new(Prelude::CT_failed,[])).content)
-    when 3 # OPERATION
-      var5.H()
-      expr.H()
-    when 4 # "[]" => CT__5B_5D
-      rhs = var2
-      expr.replace(rhs.content)
-      if expr.content.symbol.token == OPERATION
-        expr.H()
+    loop {
+      case var5.content.symbol.token
+      when 0 # VARIABLE
+        raise 'Handling Variables not implemented yet'
+      when 1, 3 # CHOICE, OPERATION
+        var5.H
+        next
+      when 2 # FAIL
+        expr.replace(CT_External::FAILED.content)
+        return expr
+      when 4 # "[]" => CT__5B_5D
+        rhs = var2
+        expr.replace(rhs.content)
+        if expr.content.symbol.token == OPERATION
+          expr.H()
+        end
+        return expr
+      when 5 # ":" => CT__3A
+        var3 = var5.content.arguments[0]
+        var4 = var5.content.arguments[1]
+        rhs = CT_Expressions::Box.new(CT_Expressions::Application.new(Prelude::CT__3A,[var3, CT_Expressions::Box.new(CT_Expressions::Application.new(Append::CT_append,[var4, var2]))]))
+        expr.replace(rhs.content)
+        return expr
       end
-    when 5 # ":" => CT__3A
-      var3 = var5.content.arguments[0]
-      var4 = var5.content.arguments[1]
-      rhs = Box.new(Application.new(Prelude::CT__3A,[var3, Box.new(Application.new(Append::CT_append,[var4, var2]))]))
-      expr.replace(rhs.content)
-    end
+    }
   end
 
   def CT_main.H(expr) # "main"
-    rhs = Box.new(Application.new(Append::CT_append,[Box.new(Application.new(Prelude::CT__3A,[make_int(1), Box.new(Application.new(Prelude::CT__3A,[make_int(2), Box.new(Application.new(Prelude::CT__5B_5D,[]))]))])), Box.new(Application.new(Prelude::CT__3A,[make_int(3), Box.new(Application.new(Prelude::CT__3A,[make_int(4), Box.new(Application.new(Prelude::CT__5B_5D,[]))]))]))]))
+    rhs = CT_Expressions::Box.new(CT_Expressions::Application.new(Append::CT_append,[CT_Expressions::Box.new(CT_Expressions::Application.new(Prelude::CT__3A,[make_int(1), CT_Expressions::Box.new(CT_Expressions::Application.new(Prelude::CT__3A,[make_int(2), CT_Expressions::Box.new(CT_Expressions::Application.new(Prelude::CT__5B_5D,[]))]))])), CT_Expressions::Box.new(CT_Expressions::Application.new(Prelude::CT__3A,[make_int(3), CT_Expressions::Box.new(CT_Expressions::Application.new(Prelude::CT__3A,[make_int(4), CT_Expressions::Box.new(CT_Expressions::Application.new(Prelude::CT__5B_5D,[]))]))]))]))
     expr.replace(rhs.content)
     expr.H()
+    return expr
   end
 
-  repl(Box.new(Application.new(Append::CT_main,[])))
+  repl(CT_Expressions::Box.new(CT_Expressions::Application.new(Append::CT_main,[])))
 
 end
